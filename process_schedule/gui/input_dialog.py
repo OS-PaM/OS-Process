ebf2240822bc8f90d9cade8148f8464aa2cd93f6
XEarthlydust <1767240342@qq.com>
2023-06-22 14:17:53 +0800

    fix(`gui`): 修正个别输入错误
    修正 输入数据未进行格式转化 及 重新初始化时未清除旧数据

--- process_schedule/gui/input_dialog.py
+++ process_schedule/gui/input_dialog.py
@@ -2,13 +2,13 @@ from PyQt5.QtCore import QRegExp
 from PyQt5.QtGui import QRegExpValidator
 from PyQt5.QtWidgets import QDialog
 
-from process_schedule.core import Timeline, PCB
+from process_schedule.core import PCB
 from process_schedule.gui.ui.ui_dialog import Ui_Dialog
 
 
 class in_dialog(QDialog, Ui_Dialog):
     pcb_list = []
-    pid: int
+    pid: int = 0
 
     def __init__(self):
         super(in_dialog, self).__init__()
@@ -24,11 +24,13 @@ class in_dialog(QDialog, Ui_Dialog):
         pass
 
     def next_button_click(self):
+        if self.pid != len(self.pcb_list):  # pid 和 pcb_list长度不一致时 证明重新进行了初始化 需要清空
+            self.pcb_list = []
         arr_time = self.arr_line.text()
         run_time = self.run_line.text()
         if arr_time != "" and run_time != "" and int(run_time):
             self.pid = self.pid + 1
-            pcb = PCB(self.arr_line.text(), self.run_line.text())
+            pcb = PCB(int(self.arr_line.text()), int(self.run_line.text()))
             self.pcb_list.append(pcb)
             self.pid_label.setText("PID " + str(self.pid) + "\t上一项 (" + "PID " + str(self.pid - 1) + ") 已记录")
         else:
@@ -36,6 +38,12 @@ class in_dialog(QDialog, Ui_Dialog):
             self.setWindowTitle("内容不能为 Null or 0")
         pass
 
+    def exec_(self):
+        return self.pcb_list
+
     def enter_button_click(self):
         self.hide()
         pass
+
+    def get_pcb_list(self):
+        return self.pcb_list
